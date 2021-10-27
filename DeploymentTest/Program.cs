0fbc6e3115df8bc5694e793728e8125c26c57c54
Douglas S <94dousch@gafe.molndal.se>
2021-10-27 10:32:50 +0200

    Add project files.

---
+++ DeploymentTest/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace DeploymentTest
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
