a91045ccec3b267c7970a484cd94f8680b6563c3
Douglas S <94dousch@gafe.molndal.se>
2021-10-27 13:59:00 +0200

    asd

--- DeploymentTest/Program.cs
+++ DeploymentTest/Program.cs
@@ -6,7 +6,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello everybody second World!");
+            Console.WriteLine("Hello everybody second and third World!");
         }
     }
 }
